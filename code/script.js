acb37881b8b1a124e3693002e7bf5537bd4ad91a
Carlo <carlobenettiwork@gmail.com>
2024-05-28 09:29:38 -0400

    Parallax ended

--- code/script.js
+++ code/script.js
@@ -6,10 +6,11 @@ let hill5 = document.getElementById('hill5');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    text.style.marginTop = value = 2.5 + 'px';
-    leaf.style.top = value = -1.5 + 'px';
-    leaf.style.left = value = 1.5 + 'px';
-    hill5.style.left = value = 1.5 + 'px';
-    hill4.style.left = value = -1.5 + 'px';
-    hill1.style.Top = value = -1 + 'px';
+    
+    text.style.marginTop = value * 2.5 + 'px';
+    leaf.style.top = value * -1.5 + 'px';
+    leaf.style.left = value * 1.5 + 'px';
+    hill5.style.left = value * 1.5 + 'px';
+    hill4.style.left = value * -1.5 + 'px';
+    hill1.style.Top = value * -1 + 'px';
 });
